4a109940ea8a5a9b93b7db2537b14a5bf6d5ea84
Shedrack <nwoyeshedrack@gmail.com>
2022-05-26 08:21:40 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Check if a word is an anagrams 
+# Example:
+# find_anagrams("hello") --> False
+# find_anagrams("racecar") --> True
+
+
+def find_anagrams(str1, str2):
+    # [assignment] Add your code here
+    str1_list = list(str1)
+    str1_list.sort()
+    str2_list = list(str2)
+    str2_list.sort()
+
+    return (str1_list == str2_list)
+
+
